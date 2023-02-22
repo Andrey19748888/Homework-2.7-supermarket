8fa62c0756fc0f3ddb2de43934fc94f331296ef6
Andreydenisov7788 <asasa88881974@mail.ru>
2023-02-22 19:31:07 +0300

    касса супермаркета

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,44 @@
 public class Main {
     public static void main(String[] args) {
+        Supermarket market = new Supermarket();
+        market.addToQueue1("Ben","Paul","Dan");
+        market.addToQueue2("Ben","Paul","Dan","Mila");
+
+        showQueues(market);
+
+        market.add("Ben");
+
+        showQueues(market);
+
+        market.add("Коля");
+
+        showQueues(market);
+
+        market.add("Александр");
+
+        showQueues(market);
+        market.add("Алекс");
+
+        showQueues(market);
+
+        market.removePerson();
+        showQueues(market);
+        market.removePerson();
+        showQueues(market);
+        market.removePerson();
+        showQueues(market);
+        market.removePerson();
+        showQueues(market);
+
+
+
 
     }
+
+    public static void showQueues(Supermarket market) {
+        System.out.println(market.getQueue1());
+        System.out.println(market.getQueue2());
+        System.out.println();
+    }
+
 }
